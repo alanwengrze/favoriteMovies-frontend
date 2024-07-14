f0a5019fc159b352ce86a2b623a8ec0c85499468
Alan Wengrze <alanwengrzee@gmail.com>
2024-07-14 04:23:40 -0300

    :bug: fix: headers

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://movies-api-32us.onrender.com",
-  withCredentials: true
+  // baseURL: "https://movies-api-32us.onrender.com",
+  baseURL: "http://localhost:3000",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  }
 });
