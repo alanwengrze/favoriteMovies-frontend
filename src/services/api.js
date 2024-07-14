81f17378f198f99482ef1a468468084075fcd9e8
Alan Wengrze <alanwengrzee@gmail.com>
2024-07-14 03:54:56 -0300

    :bug: fix: change url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://movies-api-32us.onrender.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://movies-api-32us.onrender.com",
   withCredentials: true
 });
